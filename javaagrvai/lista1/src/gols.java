b44aa8e20dfdb9c6019c3c2b45b1cfa30c3aaa44
Emily <ramos.emilyvi@gmail.com>
2025-01-30 16:40:11 -0300

    ex 4

--- javaagrvai/lista1/src/gols.java
+++ javaagrvai/lista1/src/gols.java
@@ -4,6 +4,21 @@ public class gols {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("digite a quantidade de gols do corintians: ");
+        int gols1 = scanner.nextInt();
+        int corintia = gols1;
+
+        System.out.print("digite a quantidade de gols da Ponte preta:  ");
+        int gols2 = scanner.nextInt();
+        int ponte = gols2;
+
+        if (corintia < ponte){
+            System.out.println("macaca ganhouuuuu");
+        } else if (corintia == ponte) {
+            System.out.println("empatou vei");
+        }else {
+            System.out.println("vaii corintiaa");
+        }
     }
 }
 
