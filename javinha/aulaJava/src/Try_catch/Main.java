12a083bca20c983dd1e2402f5d2088f9a37bdc8a
Ramos Emily (CaP/ETS2) <rme4ca@bosch.com>
2025-09-25 09:33:52 -0300

    refactor(paste): reorganize pastes

--- javinha/aulaJava/src/Try_catch/Main.java
+++
@@ -1,7 +0,0 @@
-package Try_catch;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("aff");
-    }
-}
--- piton/FastAPI/API/random_generators/generators/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GeneratorsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'generators'
