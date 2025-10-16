a8f118849f4a0b48bbde8d1fbca234a9844a7b58
ramos-emily <ramos.emilyvi@gmail.com>
2025-10-16 09:22:08 -0300

    feat(page-conteudo): backlog

--- next/kanban/src/app/pages/Home/page.tsx
+++ next/kanban/src/app/pages/Home/page.tsx
@@ -6,6 +6,12 @@ export default function Conteudo(){
                 <h1 className="mt-20 ml-10 text-3xl font-bold">Studio Board</h1>
                 <button className="mt-20 ml-5 -rotate-270 "> &gt; </button>
             </div>
+            <div className="grid grid-cols-4 gap-5 my-10 ml-10">
+                <div className="bg-gray-100 p-3 flex justify-between">
+                    <h4 className="flex justify-between items-center">Backlog</h4>
+                    <button className=" -rotate-270">...</button>
+                </div>
+            </div>
         </div>
         </>
     )
